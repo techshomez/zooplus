cd18969fb997ec9d90546758040d8ef1f7aba8d5
techshomez <danieltopeg>
2022-05-29 21:58:39 +0100

    fixing bugs on the cart page

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import './cart.css';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { deleteCart } from '../../redux/action/action';
 
 const Cart = () => {
+
+    const state = useSelector((state)=> state.addItem);
+    const dispatch = useDispatch();
+
+    const cartItems = (cartItems) => {
+        return(
+            <div className="px-4 my-5 bg-light rounded-3">
+                <div className="container py-4">
+                    <button className="btn"></button>
+                    <div className="row">
+                        <div className="col-md-4">
+                            <h3></h3>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
   return (
-    <div>Cart</div>
-  )
+    <>
+       {state.length !== 0 && state.map(cartItems)}
+    </>
+  );
 }
 
 export default Cart
