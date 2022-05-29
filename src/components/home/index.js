d4ce74879779b4898878b62638b60d0163561881
techshomez <danieltopeg>
2022-05-29 19:09:16 +0100

    designing the layout and fetching products from the API

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,14 @@
 import "./home.css";
 import hero_img from '../../assets/img_1.webp';
+import Products from "../product";
 
 const Home = () => {
   return (
     <div className="hero">
-      <div class="card bg-dark text-white border-0">
-        <img src={hero_img} class="card-img" alt="hero image" height="550px" />
+      <div className="card bg-dark text-white border-0">
+        <img src={hero_img} className="card-img" alt="hero image" height="550px" />
       </div>
+      <Products />
     </div>
   );
 };
--- src/components/product/index.js
+++ src/components/product/index.js
@@ -1,10 +1,82 @@
-import { useState, useEffect } from 'react';
-import './product.css';
+import React, { useState, useEffect } from "react";
+import "./product.css";
 
 const Products = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [filter, setFilter] = useState(data);
+  let componentMounted = true;
+
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true);
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (componentMounted) {
+        setData(await response.clone().json());
+        setFilter(await response.json());
+        setLoading(false);
+        console.log(filter);
+      }
+      return () => {
+        componentMounted = false;
+      };
+    };
+    getProducts();
+  }, []);
+
+  const Loading = () => {
+    return <>Loading....</>;
+  };
+
+  const ShowProducts = () => {
+    return (
+      <>
+        <div className="buttons d-flex justify-content-center mb-5 pb-5">
+          <button className="btn btn-outline-dark me-2">All</button>
+          <button className="btn btn-outline-dark me-2">Men's Clothing</button>
+          <button className="btn btn-outline-dark me-2">
+            Women's Clothing
+          </button>
+          <button className="btn btn-outline-dark me-2">Jewelry</button>
+          <button className="btn btn-outline-dark me-2">Electronics</button>
+        </div>
+        {filter.map((product) => {
+          return (
+            <>
+              <div className="col-md-3">
+                <div class="card h-100 text-center p-4" key={product.id}>
+                  <img src={product.image} class="card-img-top" alt={product.title} />
+                  <div class="card-body">
+                    <h5 class="card-title">{product.title}</h5>
+                    <p class="card-text">
+                      ${product.price}
+                    </p>
+                    <a href="#" class="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </>
+    );
+  };
+
   return (
-    <div>Products</div>
+    <div className="container my-5 py-5">
+      <div className="row">
+        <div className="col-12 mb-5">
+          <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
+          <hr />
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        {loading ? <Loading /> : <ShowProducts />}
+      </div>
+    </div>
   );
-}
+};
 
 export default Products;
